db6378350c1676e317b7a543003136352c8ea1b9
KaraniAbdellah <abdellahkarani@gmail.com>
2025-12-15 20:24:17 +0100

    feat: From C to CPP

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// GOAL: build a Inference Engine (FORWARD CHAINING, BACKWARD CHAINING)
+/* STEPS:
+    --> Knowldge Base = Fact + Rules
+    - Facts: From User ['A', 'B', 'C', 'D']
+    - Rules: [{'R1': ['A', 'B', 'C']}, {'R2': ['S', 'D', 'A']}, ...]
+*/
+
+
+#include <iostream>
+using namespace std;
+int main () {
+  cout << "Hello World! ";
+  cout << "I'm a C++ program";
+  return 0;
+}
+
+
